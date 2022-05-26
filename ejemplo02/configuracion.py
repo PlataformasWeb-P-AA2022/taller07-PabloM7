aa949927da8cef3876294dbb5ef38754317e851e
PabloM7 <pamontano1998@gmail.com>
2022-05-25 20:52:08 -0500

    Taller07

--- ejemplo02/configuracion.py
+++ ejemplo02/configuracion.py
@@ -3,9 +3,9 @@
 # cadena conector a la base de datos
 #
 # Sqlite
-cadena_base_datos = 'sqlite:///base001.db' 
+# cadena_base_datos = 'sqlite:///base001.db' 
 
 # Mysql
 # para el uso de este dialecto en SqlAlchemy
 # instalar "pip install PyMySQL"
-# cadena_base_datos = 'mysql+pymysql://usuario:clave@localhost/bd_name'
+cadena_base_datos = 'mysql+pymysql://usuario:clave@localhost/bd_name'
